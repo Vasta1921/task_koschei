c066aeb5cb45307ddf7366eca806ab861f19f009
Vasta1921 <55996682+Vasta1921@users.noreply.github.com>
2021-07-07 16:53:43 +0300

    Initial commit

---
+++ src/main/java/koschei/models/Deth8.java
@@ -0,0 +1,12 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Deth8 {
+
+    public String toString(){
+        return "смерть Кощея";
+    }
+
+}
